f178623f7fc98ce7a5f1f1486f4c2c4f19b4aaee
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2023-02-03 03:02:44 +0530

    Update WeightConverter.py

--- WeightConverter.py
+++ WeightConverter.py
@@ -1,1 +1,10 @@
+weight = int(input("Enter weight: "))
+unit = input("Enter units: (L) for pounds or (K) for kilograms: ")
+if unit.upper() == "L":
+    converted = weight * 0.45
+    print(f"Converted weight is {converted} kilograms.")
+else:
+    converted = weight / 0.45
+    print(f"Converted weight is {converted} pounds.")
 
+    
