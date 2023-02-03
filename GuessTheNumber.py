e353e2a59bcabd5d3f4f645161e7279052a1fb76
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2023-02-03 17:55:08 +0530

    Update GuessTheNumber.py

--- GuessTheNumber.py
+++ GuessTheNumber.py
@@ -1,1 +1,11 @@
-
+secretNumber = 9;
+count = 0
+limit = 3
+while count<limit:
+    guessedNumber = int(input("Enter your guess: "))
+    count=count+1
+    if guessedNumber == secretNumber:
+        print("You won!")
+        break
+else:
+    print("You lost!")
