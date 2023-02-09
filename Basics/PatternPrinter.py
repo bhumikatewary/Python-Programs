767a9e7eab75543e2b3b0bdcf40a4e77be16fe44
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2023-02-10 01:48:54 +0530

    Update PatternPrinter.py

--- Basics/PatternPrinter.py
+++ Basics/PatternPrinter.py
@@ -7,6 +7,12 @@ for i in numbers:
         output+='x'
     print(output)
     
+    #OR 
+    
+    numbers=[5,2,5,2,2]
+for i in numbers:
+    print ('x'*i)
+    
 # L Pattern printer
 numbers2=[2,2,2,2,5,5]
 for i in numbers2:
