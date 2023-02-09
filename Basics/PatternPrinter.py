ec91038e157ea2bd4fbae45c24b2c715bc3f9793
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2023-02-10 01:47:42 +0530

    Create PatternPrinter.py

---
+++ Basics/PatternPrinter.py
@@ -0,0 +1,18 @@
+# F pattern printer
+
+numbers=[5,2,5,2,2]
+for i in numbers:
+    output=''
+    for j in range (i):
+        output+='x'
+    print(output)
+    
+# L Pattern printer
+numbers2=[2,2,2,2,5,5]
+for i in numbers2:
+    output=''
+    for j in range (i):
+        output+='x'
+    print(output)
+    
+      
