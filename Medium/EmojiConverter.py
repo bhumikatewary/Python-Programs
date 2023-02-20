bdb390ddef28bccd9ec4a0947f9b975904cca08e
Bhumika Tewary <77784592+bhumikatewary@users.noreply.github.com>
2023-02-20 13:07:37 +0530

    Update EmojiConverter.py

--- Medium/EmojiConverter.py
+++ Medium/EmojiConverter.py
@@ -11,3 +11,26 @@ output=""
 for word in words:
     output+=emojis.get(word, word)+" "
 print(output)
+
+
+## Creating reusable code:
+
+
+def emoji_converter(msg):
+    words=msg.split(' ') #if it encounters a space, it will split the msg accordingly
+    emojis = {
+    "blessed":"😇",
+    "happy":"😄",
+    "cool":"😎",
+    "angry":"😡",
+    "sad":"😞"
+}
+    output=""
+    for word in words:
+        output+=emojis.get(word, word)+" "
+    return output
+        
+        
+msg = input("Enter your message -> ").lower()
+result=emoji_converter(msg)
+print(result)
